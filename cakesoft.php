bde95a34893bdbe5e59007c1e7cf6c9f424361ef
Anant <38176971+ambreanant@users.noreply.github.com>
2020-10-30 12:12:09 +0530

    cakesoft
    cakesoft

--- cakesoft.php
+++ cakesoft.php
@@ -12,6 +12,9 @@ while ( --$x > 0) {
 // Having COUNT(*) > 1
 
 // select * from person where daily_avg = (select max(daily_avg) from person) GROUP by person_name Having count(*) > 1
+<!-- select * 
+from (select * from Person order by `person_name`, daily_avg desc, id) x
+group by `person_name` -->
 
 <?php
 $a = 1;
